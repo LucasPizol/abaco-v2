b53fc41072f9a837e3d2b55c4d67e27418a35bed
Mariana <mariana.tessarini@sou.unifeob.edu.br>
2024-10-15 22:36:57 -0300

    modificações tela login e inicio

--- src/app/common/main/main.component.ts
+++ src/app/common/main/main.component.ts
@@ -4,12 +4,14 @@ import { ImgAddCircleComponent } from "../../components/img-add-circle/img-add-c
 import { ImgNoteAddComponent } from "../../components/img-note-add/img-note-add.component";
 import { ImgPagePreviewComponent } from "../../components/img-page-preview/img-page-preview.component";
 import { ReorganizesMainComponent } from "../../components/reorganizes-main/reorganizes-main.component";
+import { TitlesComponent } from "../../components/titles/titles.component";
+import { NavLinkComponent } from "../../components/nav-link/nav-link.component";
 
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CardComponent, ImgAddCircleComponent, ImgNoteAddComponent, ImgPagePreviewComponent, ReorganizesMainComponent],
+  imports: [CardComponent, ImgAddCircleComponent, ImgNoteAddComponent, ImgPagePreviewComponent, ReorganizesMainComponent, TitlesComponent, NavLinkComponent],
   templateUrl: './main.component.html',
 })
 export class MainComponent {
