d31fb55e6f2398ff951fac8a756b336afad13db0
aluno <aluno@UNIFEOB-ACAD.LOCAL>
2024-10-23 21:58:00 -0300

    feat: inicialização da tela calendário

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
-import { routes } from './app/app.routes'; 
+import { routes } from './app/app.routes';
+import { pt_BR, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import pt from '@angular/common/locales/pt';
+import { FormsModule } from '@angular/forms';
+import { importProvidersFrom } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
+
+registerLocaleData(pt); 
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes)  
+    provideRouter(routes), provideNzI18n(pt_BR), importProvidersFrom(FormsModule), provideAnimationsAsync(), provideHttpClient()  
   ]
 }).catch(err => console.error(err));
