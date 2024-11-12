dcd0b19296577e3121a9a2724602375fa1a82988
Lucas Pizol Ferreira <lucaspizolfe@gmail.com>
2024-11-12 11:12:15 -0300

    feat: launch notas students loading

--- src/app/common/launch-notas/launch-notas.component.ts
+++ src/app/common/launch-notas/launch-notas.component.ts
@@ -1,18 +1,22 @@
-import { Component } from '@angular/core';
-import { ITableHeader } from '../../interfaces/TableHeader';
-import { GradeService } from '../../api/grade/grade.service';
-import { IGradeModel } from '../../api/grade/IGrade';
-import { IStudentModel } from '../../api/student/Istudent';
-import { ListDivComponent } from '../../components/list-div/list-div.component';
-import { ReorganizesMainComponent } from '../../components/reorganizes-main/reorganizes-main.component';
-import { TitlesComponent } from '../../components/titles/titles.component';
-import { GridComponent } from '../../components/grid/grid.component';
-import { InputsComponent } from '../../components/inputs/inputs.component';
-import { TableComponent } from '../../components/table/table.component';
-import { ImgEditComponent } from '../../components/images/img-edit/img-edit.component';
-import { ReorganizeInputComponent } from '../../components/reorganize-input/reorganize-input.component';
-import { BtnDownComponent } from '../../components/buttons/btn-down/btn-down.component';
-import { BreakpointService } from '../../services/breakpoint.service';
+import { Component } from '@angular/core'
+import { ITableHeader } from '../../interfaces/TableHeader'
+import { GradeService } from '../../api/grade/grade.service'
+import { IGradeModel } from '../../api/grade/IGrade'
+import { IStudentModel } from '../../api/student/Istudent'
+import { ListDivComponent } from '../../components/list-div/list-div.component'
+import { ReorganizesMainComponent } from '../../components/reorganizes-main/reorganizes-main.component'
+import { TitlesComponent } from '../../components/titles/titles.component'
+import { GridComponent } from '../../components/grid/grid.component'
+import { InputsComponent } from '../../components/inputs/inputs.component'
+import { TableComponent } from '../../components/table/table.component'
+import { ImgEditComponent } from '../../components/images/img-edit/img-edit.component'
+import { ReorganizeInputComponent } from '../../components/reorganize-input/reorganize-input.component'
+import { BtnDownComponent } from '../../components/buttons/btn-down/btn-down.component'
+import { BreakpointService } from '../../services/breakpoint.service'
+import { ICourseModel } from '../../api/course/ICourse'
+import { CourseService } from '../../api/course/course.service'
+import { CommonModule } from '@angular/common'
+import { FormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-launch-notas',
@@ -27,26 +31,37 @@ import { BreakpointService } from '../../services/breakpoint.service';
     ImgEditComponent,
     ReorganizeInputComponent,
     BtnDownComponent,
+    CommonModule,
+    FormsModule,
   ],
   templateUrl: './launch-notas.component.html',
-  providers: [BreakpointService],
+  providers: [BreakpointService, CourseService],
 })
 export class LaunchNotasComponent {
   constructor(
     private gradeService: GradeService,
-    private readonly breakpointService: BreakpointService
+    private readonly breakpointService: BreakpointService,
+    private readonly courseService: CourseService
   ) {}
 
-  data: IStudentModel[] = [];
+  data: IStudentModel[] = []
 
-  imgEdit = '<app-img-edit></app-img-edit>';
+  courses: ICourseModel[] = []
+
+  imgEdit = '<app-img-edit></app-img-edit>'
+
+  formData = {
+    curso_id: '',
+    aula: undefined,
+    descricao: '',
+  }
 
   async ngOnInit() {
-    await this.loadGrades();
+    await this.loadCourses()
   }
 
   fetchHeaderByBreakpoint(): ITableHeader<IStudentModel> {
-    const { xs, sm, md } = this.breakpointService.breakpoint();
+    const { xs, sm, md } = this.breakpointService.breakpoint()
 
     if (xs) {
       return [
@@ -54,7 +69,7 @@ export class LaunchNotasComponent {
         { key: 'nome', label: 'Nome', className: 'w-1/3' },
         { key: 'nota', label: 'Nota', className: 'w-1/3' },
         { key: 'situacao', label: 'Situação', className: 'w-1/3' },
-      ];
+      ]
     }
 
     return [
@@ -62,15 +77,28 @@ export class LaunchNotasComponent {
       { key: 'nome', label: 'Nome Completo', className: 'w-1/4 text-center' },
       { key: 'nota', label: 'Nota', className: 'w-1/4 text-center' },
       { key: 'situacao', label: 'Situação', className: 'w-1/4 text-center' },
-    ];
+    ]
   }
 
   async loadGrades() {
     try {
-      const data = await this.gradeService.getGrades(1);
-      this.data = data;
+      const data = await this.gradeService.getGrades(parseInt(this.formData.curso_id))
+      this.data = data
     } catch (error) {
-      console.error('Erro ao carregar faltas:', error);
+      console.error('Erro ao carregar faltas:', error)
     }
+  }
+
+  async loadCourses() {
+    try {
+      const data = await this.courseService.getCourses()
+      this.courses = data
+    } catch (error) {
+      console.error('Erro ao carregar cursos:', error)
+    }
+  }
+
+  async onSubmit(form: any) {
+    console.log(this.formData)
   }
 }
