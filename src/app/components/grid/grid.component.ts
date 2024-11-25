262188f809062d6f7c6cd42ecac8e389da97ffa0
Lucas Pizol Ferreira <lucaspizolfe@gmail.com>
2024-11-25 10:49:42 -0300

    fix: grid

--- src/app/components/grid/grid.component.ts
+++ src/app/components/grid/grid.component.ts
@@ -1,14 +1,15 @@
-import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common'
+import { Component, Input } from '@angular/core'
 
 @Component({
   selector: 'app-grid',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './grid.component.html',
 })
 export class GridComponent {
-  @Input() xs: number | undefined = 1;
-  @Input() sm: number | undefined = 1;
-  @Input() md: number | undefined = 2;
-  @Input() lg: number | undefined = 2;
+  @Input() xs: number | undefined = 1
+  @Input() sm: number | undefined = 1
+  @Input() md: number | undefined = 2
+  @Input() lg: number | undefined = 2
 }
